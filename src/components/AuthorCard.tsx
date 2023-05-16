f353c3f48b3b36f58489405e0879243dc300768a
Norbert <cross@linux.pl>
2023-05-16 16:36:56 +0200

    Add negative margin to avatar in profile card

--- src/components/AuthorCard.tsx
+++ src/components/AuthorCard.tsx
@@ -16,9 +16,9 @@ interface Props {
 
 function AuthorCard({ author }: Props) {
   return (
-    <Box>
+    <Box className='mt-20'>
       <div className='flex flex-col items-center'>
-        <img className='mb-6 rounded-full border-2 border-white shadow-2xl' width={128} height={128} src={author.avatarUrl ? author.avatarUrl : 'https://placehold.co/128x128'}/>
+        <img className='-mt-20 mb-6 rounded-full border-2 border-white shadow-2xl' width={128} height={128} src={author.avatarUrl ? author.avatarUrl : 'https://placehold.co/128x128'}/>
         <h2 className='font-semibold text-gray-800 m-0 mb-8'>{author.username}</h2>
 
         <div className='flex justify-around w-full md:w-1/2 mb-4'>
