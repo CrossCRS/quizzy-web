7be385e7265947755636fcfc9fbeb534721ae439
Norbert Budzynski <cross@linux.pl>
2023-05-23 11:58:04 +0200

    Adjust data skeletons contrast

--- src/components/AuthorCardSkeleton.tsx
+++ src/components/AuthorCardSkeleton.tsx
@@ -6,15 +6,15 @@ function AuthorCardSkeleton() {
   return (
     <Box className='mt-20'>
       <div className='flex flex-col items-center'>
-        <div className='-mt-20 mb-6 w-32 h-32 bg-gray-300 rounded-full border-2 border-white shadow-2xl'/>
-        <span className='m-0 mb-8 w-48 h-8 bg-gray-300 animate-pulse'></span>
+        <div className='-mt-20 mb-6 w-32 h-32 bg-gray-200 rounded-full border-2 border-white shadow-2xl'/>
+        <span className='m-0 mb-8 w-48 h-8 bg-gray-200 animate-pulse'></span>
 
         <div className='flex justify-around w-full md:w-1/2 mb-4'>
           <div className='flex flex-col items-center'>
-            <span className='w-32 h-16 bg-gray-300 animate-pulse'></span>
+            <span className='w-32 h-16 bg-gray-200 animate-pulse'></span>
           </div>
           <div className='flex flex-col items-center'>
-            <span className='w-32 h-16 bg-gray-300 animate-pulse'></span>
+            <span className='w-32 h-16 bg-gray-200 animate-pulse'></span>
           </div>
         </div>
       </div>
--- src/components/QuizCardSkeleton.tsx
+++ src/components/QuizCardSkeleton.tsx
@@ -11,10 +11,10 @@ function QuizCardSkeleton(props: Props) {
     <>
       {[...Array(props.count)].map((_, i) =>
         <Box key={`quizcardskeleton-${i}`}>
-          <span className='my-1 h-8 w-64 bg-gray-300 animate-pulse'></span>
-          <span className='my-1 h-4 w-48 bg-gray-300 animate-pulse'></span>
-          <span className='my-4 h-24 w-full bg-gray-300 animate-pulse'></span>
-          <span className='my-1 h-10 w-full bg-gray-300 animate-pulse'></span>
+          <span className='my-1 h-8 w-64 bg-gray-200 animate-pulse'></span>
+          <span className='my-1 h-4 w-48 bg-gray-200 animate-pulse'></span>
+          <span className='my-4 h-24 w-full bg-gray-200 animate-pulse'></span>
+          <span className='my-1 h-10 w-full bg-gray-200 animate-pulse'></span>
         </Box>,
       )}
     </>
