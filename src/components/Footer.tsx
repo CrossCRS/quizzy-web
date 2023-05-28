f78d79dd9f4bc8202dfbf42c59f6b529fc791731
Norbert <cross@linux.pl>
2023-05-28 21:50:16 +0200

    Update footer socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { faTwitter, faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Footer() {
@@ -9,17 +9,11 @@ function Footer() {
   return (
     <footer className="flex flex-col items-center w-full p-6 bg-emerald-500 text-white">
       <nav className="flex">
-        <a href="/" className={classSocial}>
-          <FontAwesomeIcon icon={faTwitter} />
+        <a href="http://github.com/CrossCRS/" target="_blank" rel="noreferrer" className={classSocial}>
+          <FontAwesomeIcon icon={faGithub} />
         </a>
-        <a href="/" className={classSocial}>
-          <FontAwesomeIcon icon={faFacebook} />
-        </a>
-        <a href="/" className={classSocial}>
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        <a href="/" className={classSocial}>
-          <FontAwesomeIcon icon={faYoutube} />
+        <a href="https://www.linkedin.com/in/norbert-budzy%C5%84ski-b75885218" target="_blank" rel="noreferrer" className={classSocial}>
+          <FontAwesomeIcon icon={faLinkedin} />
         </a>
       </nav>
 
